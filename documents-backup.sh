d8e0dd7192a2cf238a2f859dbaed3c512615300e
Stian Toftevåg <stiantoftevaag@gmail.com>
2021-10-08 20:35:26 +0200

    Added $USER in save path

--- documents-backup.sh
+++ documents-backup.sh
@@ -6,7 +6,7 @@ USER=$(whoami)
 cd /home/"$USER"
 echo Making backup of "$USER" documents folder.
 sleep 2 
-tar czvf /home/stiantoftevag/backup/popos-docs-backup-"$TODAY".tgz Documents 
+tar czvf /home/"$USER"/backup/popos-docs-backup-"$TODAY".tgz Documents 
 echo Sending Backup to Boxi
 sleep 2
 scp /home/"$USER"/backup/popos-docs-backup-"$TODAY".tgz "$USER"@192.168.178.50:/home/"$USER"/backup/popos
