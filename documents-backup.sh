e063a6b79f671aa47f860e70b3d74552777030a2
Stian Toftevåg <stiantoftevaag@gmail.com>
2021-10-09 20:17:47 +0200

    House keeping on script

--- documents-backup.sh
+++ documents-backup.sh
@@ -1,14 +1,11 @@
-#! /bin/sh
+#!/bin/sh
 
 TODAY=$(date +"%F")
 USER=$(whoami)
 
 cd /home/"$USER"
 echo Making backup of "$USER" documents folder.
-sleep 2 
 tar czvf /home/stiantoftevag/backup/popos-docs-backup-"$TODAY".tgz Documents 
 echo Sending Backup to Boxi
-sleep 2
 scp /home/"$USER"/backup/popos-docs-backup-"$TODAY".tgz "$USER"@192.168.178.50:/home/"$USER"/backup/popos
 echo All done!
-sleep 2
