8890a9e41ee860907c6292ad9774d383fa81c514
Stian Toftevåg <stiantoftevaag@gmail.com>
2021-10-12 17:33:43 +0200

    Cleaning up script after merge error

--- documents-backup.sh
+++ documents-backup.sh
@@ -5,12 +5,7 @@ USER=$(whoami)
 
 cd /home/"$USER"
 echo Making backup of "$USER" documents folder.
-<<<<<<< HEAD
-tar czvf /home/stiantoftevag/backup/popos-docs-backup-"$TODAY".tgz Documents 
-=======
-sleep 2 
-tar czvf /home/"$USER"/backup/popos-docs-backup-"$TODAY".tgz Documents 
->>>>>>> 32e33997f5209fad1cd6eb27fa573bdc28fcbfd2
+tar czvf /home/"$USER"/backup/popos-docs-backup-"$TODAY".tgz Documents
 echo Sending Backup to Boxi
 scp /home/"$USER"/backup/popos-docs-backup-"$TODAY".tgz "$USER"@192.168.178.50:/home/"$USER"/backup/popos
 echo All done!
