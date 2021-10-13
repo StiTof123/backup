20fd7479accf80e385e3f53ff02cf1f54b2dd47b
Stian Toftevåg <stiantoftevaag@gmail.com>
2021-10-13 17:29:42 +0200

    Adding universal user implementation

--- documents-backup.sh
+++ documents-backup.sh
@@ -2,12 +2,15 @@
 
 TODAY=$(date +"%F")
 USER=$(whoami)
+NODE=$(uname -n)
 
 cd /home/"$USER"
 echo Making backup of "$USER" documents folder.
-tar czvf /home/"$USER"/backup/popos-docs-backup-"$TODAY".tgz Documents
+mkdir /home/"$USER"/backup/
+tar czvf /home/"$USER"/backup/"$NODE"-docs-backup-"$TODAY".tgz Documents
 find /home/"$USER"/backup/ -type f -mtime +5 -exec rm {} \;
 echo Sending Backup to Boxi
-scp /home/"$USER"/backup/popos-docs-backup-"$TODAY".tgz "$USER"@192.168.178.50:/home/"$USER"/backup/popos
-ssh "$USER"@192.168.178.50 'find /home/"$USER"/backup/popos -type f -mtime +10 -exec rm {} \;' 
+ssh -o SendEnv=$NODE "$USER"@192.168.178.50 mkdir backup/"$NODE"
+scp /home/"$USER"/backup/"$NODE"-docs-backup-"$TODAY".tgz "$USER"@192.168.178.50:/home/"$USER"/backup/"$NODE"
+ssh "$USER"@192.168.178.50 'find /home/"$USER"/backup/"$NODE" -type f -mtime +10 -exec rm {} \;' 
 echo All done!
