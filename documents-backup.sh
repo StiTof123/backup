feed520c02422cf3207eae822cb68a3c9e7046f7
Stian Toftevåg <stiantoftevaag@gmail.com>
2021-10-13 15:13:12 +0200

    added hosekeeping on boxi

--- documents-backup.sh
+++ documents-backup.sh
@@ -9,4 +9,5 @@ tar czvf /home/"$USER"/backup/popos-docs-backup-"$TODAY".tgz Documents
 find /home/"$USER"/backup/ -type f -mtime +5 -exec rm {} \;
 echo Sending Backup to Boxi
 scp /home/"$USER"/backup/popos-docs-backup-"$TODAY".tgz "$USER"@192.168.178.50:/home/"$USER"/backup/popos
+ssh "$USER"@192.168.178.50 'find /home/"$USER"/backup/popos -type f -mtime +10 -exec rm {} \;' 
 echo All done!
